e28de987d7d26551259feb74e1cb80d6c4e19ac6
Clément PRUDHOMME <clement.prudhomme@viacesi.fr>
2020-05-11 23:49:09 +0200

    Ajout method Getint

---
+++ MyConverterLibrary/MyConverterLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyConverterLibrary
+{
+    public class Class1
+    {
+        public static int GetInt(string nb)
+        {
+            return int.Parse(nb) + 1;
+        }
+    }
+}
